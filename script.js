308944a8222fa8d977352065f75d4c203e205532
JackelineMatos <jackeline.matos@gmail.com>
2024-01-16 17:55:49 +0100

    finalizacao-jogo

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-const randomNumber = Math.round(Math.random() * 10);
-let xAttemps = 1;
+const screen1 =  document.querySelector(".screen1");
+const screen2 =  document.querySelector(".screen2");
 
-function handleClick(event) {
-    event.preventDefault()
+const randomNumber = Math.round(Math.random() * 10);
+let xAttemps = 1; // variável de controle
+
+
+//Funcao Callback
+function handleTryClick(event) {
+    event.preventDefault() // nao faca o padrao do comportamento do evento
     
-    constinputNumber = document.querySelector("#inputNumber")
+    const inputNumber = document.querySelector("#inputNumber")
     
 
     if(Number(inputNumber.value) === randomNumber){
-        document.querySelector(".screen1").classList.add("hide");
-        document.querySelector(".screen2").classList.remove("hide");
+        screen1.classList.add("hide");
+        screen2.classList.remove("hide");
 
         document
-        .querySelector(".screen2 h2")
-        .innerText = `Voce acretou em ${xAttemps} tentativas`;
+            .querySelector(".screen2 h2")
+            .innerText = `Voce acertou em ${xAttemps} tentativas`;
     }
 
+    inputNumber.value = "";
     xAttemps++
 };
+
+
+//eventos
+
+const btnTry = document.querySelector("#bntTry");
+const btnReset = document.querySelector("#bntReset");
+
+//Funcao Callback
+
+btnTry.addEventListener('click', handleTryClick);
+btnReset.addEventListener('click', function name() {
+    screen1.classList.remove("hide");
+    screen2.classList.add("hide");
+    xAttemps = 1;
+});
+
 
 
